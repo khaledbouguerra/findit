4cc341ff49d386e82e7ac359ac9d357ec23d27a1
khaledbouguerra <khaledbouguerra92@gmail.com>
2016-12-16 02:09:19 +0100

    search auto-complete not finished

--- src/app/_widgets/movies/movies.component.ts
+++ src/app/_widgets/movies/movies.component.ts
@@ -9,19 +9,28 @@ import {MovieServiceService} from "../../_services/movie-service.service";
 export class MoviesComponent implements OnInit {
   searchStr:string;
   movies:Array<Object>;
-  constructor(private movieServiceService:MovieServiceService) { }
+  searching:boolean=false;
+  reaserchAutocomplete:any;
+  constructor(private movieServiceService:MovieServiceService) {}
 
   ngOnInit() {
   }
   searchMovies(){
+    this.searching=true;
     this.movieServiceService.searchMovies(this.searchStr)
       .subscribe(result=>{
         this.movies=result.results;
+        this.searching=false;
         console.log(this.movies);
 
       });
     console.log('you are now looking for a movie');
     console.log(this.searchStr);
     console.log('khaled');
+  }
+  autocompleteResult(){
+    this.movieServiceService.searchMovies(this.searchStr)
+      .subscribe(result=>{console.log(result.results[0])});
+
   }
 }
